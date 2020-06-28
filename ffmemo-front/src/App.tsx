0a8a274aca186a889f57119c4ecfa48404637b86
ryo yamaoka <selicom926@yahoo.co.jp>
2020-06-28 13:48:07 +0900

    backendとテスト連携

--- ffmemo-front/src/App.tsx
+++ ffmemo-front/src/App.tsx
@@ -1,26 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface IAppProps {}
+
+export default class App extends React.Component<IAppProps> {
+
+    constructor(props: IAppProps) {
+        super(props);
+
+        this.handleClickButton = this.handleClickButton.bind(this);
+    }
+
+    handleClickButton(event: React.MouseEvent<HTMLButtonElement>) {
+        const client = new ApolloClient({
+            uri: 'http://localhost:3000/graphql'
+        })
+        const query = gql`query {
+            memosInDB {
+                id
+                categoryId
+                title
+                content
+            }
+        }`
+
+        console.log(query);
+
+        client.query({query}).then(console.log).catch(console.log);
+        // const { loading, data} = useQuery(query);
+
+        // if (loading) return;
+
+        // console.log(data);
+    }
+
+    render() {
+        const client = new ApolloClient({
+            uri: 'http://localhost:3000/graphql'
+        })
+
+        return (
+        <ApolloProvider client={client}>
+            <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <button onClick={this.handleClickButton}>Learn React</button>
+            </header>
+        </ApolloProvider>
+        );
+    }
 }
-
-export default App;
